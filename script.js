4f69b79fdbf844e1b80d18ce1f18c231552bc62a
Daniel Young <109361146+Daniel-E-Young@users.noreply.github.com>
2023-05-23 05:37:46 +0000

    commit

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ const d = document;
 const $q = d.querySelectorAll.bind(d);
 const $g = d.querySelectorAll.bind(d);
 const $prev = $g(".prev");
-const $next = $g(".next")
+const $next = $g(".next");
+const $list = $g("")
