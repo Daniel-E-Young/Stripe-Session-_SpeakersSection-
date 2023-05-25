e0d7b21968696da09baab8c06ec513c692d35be5
Daniel Young <109361146+Daniel-E-Young@users.noreply.github.com>
2023-05-25 08:19:26 +0000

    commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const d = document; 
+const d = document;
 const $q = d.querySelectorAll.bind(d);
-const $g = d.querySelectorAll.bind(d);
+const $g = d.querySelector.bind(d);
 const $prev = $g(".prev");
 const $next = $g(".next");
 const $list = $g(".carousel__list");
@@ -32,32 +32,49 @@ const nextSlide = () => {
     $list.append($first);
     activateSlide( $q(".carousel__item")[index] );
 }
+
+const chooseSlide = (e) => {
+    const max = (window.matchMedia("screen and ( max-width: 600px)").matches) ? 5 : 8;
+    const $slide = e.target.closest( ".carousel__item" );
+    const index = getSlideIndex( $slide );
+    if ( index < 3 || index > max ) return;
+    if ( index === max ) nextSlide();
+    if ( index === 3 ) prevSlide();
+    activateSlide($slide);
+}
+
 const activateSlide = ($slide) => {
     if (!$slide) return;
     const $slides = $q(".carousel__item");
-    $slides.ForEach(el => el.removeAttrbute('data-active'));
-    $slide.setAttribute ('data-active', true);
+    $slides.forEach(el => el.removeAttribute('data-active'));
+    $slide.setAttribute( 'data-active', true );
     $slide.focus();
 }
+
 const autoSlide = () => {
     nextSlide();
 }
+
 const pauseAuto = () => {
     clearInterval( auto );
     clearTimeout( pauser );
 }
+
 const handleNextClick = (e) => {
     pauseAuto();
     nextSlide(e);
 }
+
 const handlePrevClick = (e) => {
     pauseAuto();
     prevSlide(e);
 }
+
 const handleSlideClick = (e) => {
     pauseAuto();
     chooseSlide(e);
 }
+
 const handleSlideKey = (e) => {
     switch(e.keyCode) {
         case 37:
@@ -70,13 +87,15 @@ const handleSlideKey = (e) => {
             break;
     }
 }
+
 const startAuto = () => {
     auto = setInterval( autoSlide, 3000 );
 }
+
 startAuto();
 
 $next.addEventListener( "click", handleNextClick );
 $prev.addEventListener( "click", handlePrevClick );
-$list.addEventListener( "click", handleSlideClick );
+// $list.addEventListener( "click", handleSlideClick );
 $list.addEventListener( "focusin", handleSlideClick );
 $list.addEventListener( "keyup", handleSlideKey );
